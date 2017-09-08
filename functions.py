c1ca5415e22cf5c63d860bf4ac93f075c25d4058
pomfy113 <pomfy113@gmail.com>
2017-09-07 17:42:09 -0700

    fibonacci from argv
    Can now grab fibonacci sequence from terminal

--- functions.py
+++ functions.py
@@ -1,7 +1,9 @@
+import sys
 # Solution to problem 10; fibonacci
 def iter_fib(num):
     # Keeps track of the number, what number to add next,
     # and specifically a temporary holder for switching things up
+    # fibonacci is tricky because you don't want to overwrite
     currentNum = 1
     prevNum = 0
     temp = 0
@@ -10,8 +12,11 @@ def iter_fib(num):
     if num == None:
         num = 10
 
-    # basic loop for fibonacci
-    # take whatever number is up, add the prev
+    # basic loop for fibonacci. As an example, let's say we're at 1 2 3 5
+    # take whatever number is up, add the previous number. (5+3, next num is 8)
+    # temp holds the previous number to add to the new number (temp = 3)
+    # load up the new previous number (the new prevnum is now 5)
+    # now load up the new current number (we're moving onto 3+5 or 8)
     for i in range(num):
         print currentNum+prevNum
         temp = prevNum
@@ -20,4 +25,7 @@ def iter_fib(num):
 
 # I'm not sure about defaults yet
 num = 10
+if len(sys.argv) == 2:
+    num = int(sys.argv[1])
+
 iter_fib(num)
