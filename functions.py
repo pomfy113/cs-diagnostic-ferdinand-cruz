af47d0f473cd7b4db207e4dfcc106aeeb9316ad0
pomfy113 <pomfy113@gmail.com>
2017-09-11 14:29:46 -0700

    Dice roll & probability
    Dice roll with extra credit and the occurences!

--- functions.py
+++ functions.py
@@ -1,4 +1,4 @@
-import sys
+import sys, random, re
 # Solution to problem 10; fibonacci
 def iter_fib(num):
     # Keeps track of the number, what number to add next,
@@ -25,11 +25,6 @@ def iter_fib(num):
 
 # I'm not sure about defaults yet
 
-num = 10
-if len(sys.argv) == 2:
-    num = int(sys.argv[1])
-
-#iter_fib(num)
 
 # Recursion!
 def recur_factorial(num_factorial):
@@ -41,4 +36,52 @@ def recur_factorial(num_factorial):
     else:
         return num_factorial * recur_factorial(num_factorial-1)
 
-print recur_factorial(3)
+def dice_roll():
+    # Choose how high you want the number to be
+    n = 6
+    print "Rolling dice...!", random.randrange(1,n)
+
+def mostcommon(k):
+    # LIST LIST LIST LIST list
+    # I need all of these for reasons
+    teststring = "egg cake fish egg cake chicken cake James mother father chicken egg"
+    dictionary = teststring.split()
+    alreadyplaced = []
+    currentlist = []
+    currentWord = None
+
+    for i in range(len(dictionary)):
+        # make it lower case first for ease, also non-alphanum disappear
+        dictionary[i] = filter(str.isalnum, dictionary[i].lower())
+
+        if dictionary[i] in alreadyplaced:
+            # I feel like there's a better way to use this but tuples messed me up
+            # The index in alreadyplaced will always be the same in currentlist
+            currentWord = alreadyplaced.index(dictionary[i])
+            currentlist[currentWord] = [currentlist[currentWord][0]+1, currentlist[currentWord][1]]
+        else:
+            #1st instance to initialize
+            currentlist.append([1, dictionary[i]])
+            alreadyplaced.append(dictionary[i])
+
+    # Sort the list
+    currentlist = sorted(currentlist, reverse=True)
+    # Print the list depending on k
+    for j in range(k):
+            print sorted(currentlist[j])
+
+
+
+if __name__ == "__main__":
+    # How many times you wanna run fibonacci
+    fibs = 5
+    if len(sys.argv) == 2:
+        fibs = int(sys.argv[1])
+    # What factorial you want to figure out
+    factorialnum = 3
+
+    print "Running fibonacci", fibs, "times:"
+    iter_fib(fibs)
+    print "Factorial of", factorialnum, "is", recur_factorial(factorialnum)
+    dice_roll()
+    mostcommon(3)
